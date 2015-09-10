9c2329843f7ad8a239fff6a678ccd25db65601b2
James Brennan <james@carbonfive.com>
2015-09-09 17:00:56 -0700

    Audio

--- audio.js
+++ audio.js
@@ -10,36 +10,33 @@ function duration(bpm, beats, sampleRate) {
 
 function sinetone() {
   var temp = 60;
-  var x1 = 0, y1 = temp * 10 / Pico.sampleRate;
-  var x2 = 0, y2 = temp * 10 + 5 / Pico.sampleRate;
-  var remainder = 0;
-  var noteOn = true;
+  var x1 = 0, y1 = (temp * 10) / Pico.sampleRate;
+  var x2 = 0, y2 = (temp * 10 + 5) / Pico.sampleRate;
 
   var bpm = 120;
-  var beats = 1;
+  var beats = 0.5;
+  var noteOn = true;
+
+  var remainder = 0;
+  var volume = 0.25;
 
   return function(e) {
     var out = e.buffers;
 
     remainder = remainder ? remainder : duration(bpm, beats, Pico.sampleRate);
 
-    var loopUntil = e.bufferSize;
-
-    if(e.bufferSize > remainder) {
-      loopUntil = remainder;
+    if(e.bufferSize >= remainder) {
       remainder = 0;
       noteOn = !noteOn;
     }
     else {
-      loopUntil = e.bufferSize;
       remainder -= e.bufferSize;
     }
 
-    for (var i = 0; i < loopUntil; i++) {
-      console.log(noteOn, loopUntil, remainder);
+    for (var i = 0; i < e.bufferSize; i++) {
       if(noteOn) {
-        out[0][i] = Math.sin(2 * Math.PI * x1) * 0.25;
-        out[1][i] = Math.sin(2 * Math.PI * x2) * 0.25;
+        out[0][i] = Math.sin(2 * Math.PI * x1) * volume;
+        out[1][i] = Math.sin(2 * Math.PI * x2) * volume;
         x1 += y1;
         x2 += y2;
       }
